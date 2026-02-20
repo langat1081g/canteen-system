fffd2bba3e7754c8bdedaa21bba8fd04577eab3b
Kiprono Langat <langat1081g@gmail.com>
2026-02-20 20:28:12 +0300

    changes

---
+++ supabase.js
@@ -0,0 +1,9 @@
+// supabase.js
+
+const SUPABASE_URL = "https://hbofboaoixzxqzouncyj.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Hgp6-35ojerC2XGJVfq2yg_cyTd3L4I";
+
+window.supabaseClient = window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
